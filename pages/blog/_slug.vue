6111da733b9bd68f256b61f6a1768759634768cc
bottlehs <qudgns9@gmail.com>
2020-11-13 15:42:39 +0900

    Nuxt content 🍀

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ page.title }}</h1>
+    <p>{{ page.description }}</p>
+    <nuxt-content :document="page" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    const slug = params.slug || 'index'
+    const page = await $content(slug)
+      .fetch()
+      // eslint-disable-next-line handle-callback-err
+      .catch((err) => {
+        error({ statusCode: 404, message: 'Page not found' })
+      })
+
+    return {
+      page,
+    }
+  },
+}
+</script>
